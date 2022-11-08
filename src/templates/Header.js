867806a8b4e479b2548eefbf4d5df03c28d359d8
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2022-11-08 14:55:06 -0500

    se actualizo

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -1,22 +1,21 @@
 const Header = () => {
-  const view = `
-  <div class="Header-main">
-      <div class="Header-logo">
+    const view = `
+      <div class="Header-main">
+        <div class="Header-logo">
           <h1>
-              <a href="/">
-                  Rick And Morty
-              </a>
+            <a href="/">
+              rick and morty
+            </a>
           </h1>
-      </div>
-
-      <div class="Header-nav">
+        </div>
+        <div class="Header-nav">
           <a href="#/about/">
             About
           </a>
+        </div>
       </div>
-  </div>
-  `;
-  return view;
-};
-
-export default Header;
+    `;
+    return view;
+  };
+  
+  export default Header;
