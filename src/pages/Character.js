14aa968deaa1501590b53c01311de158aa0ecab1
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2022-11-08 14:54:56 -0500

    se actualizo

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -1,26 +1,29 @@
-const Character = () => {
+import { getData } from "../utils/getData";
+import { getHash } from "../utils/getHash";
+
+const Character = async () => {
+  const id = getHash();
+  const character = await getData(id);
+
+  const { name, image, episode, status, species, gender, origin, location } =
+    character;
   const view = `
-  
-  <div class="Character-inner">
-      <article class="Characters-card">
-          <img src="image" alt="name">
-      </article>
-      <h2>Name</h2>
-
-      <article class="Character-card">
-          <h3>Episode:</h3>
-          <h3>Status:</h3>
-          <h3>Species:</h3>
-          <h3>Gender:</h3>
-          <h3>Origin:</h3>
-          <h3>Last Location:</h3>
-
-      </article>
-
-  </div>
-  
-  `
+      <div class="Characters-inner">
+        <article class="Characters-card">
+          <img src="${image}" alt="${name}">
+          <h2>${name}</h2>
+        </article>
+        <article class="Characters-card">
+          <h3>Episodes: ${episode.length}</h3>
+          <h3>Status: ${status}</h3>
+          <h3>Species: ${species}</h3>
+          <h3>Gender: ${gender}</h3>
+          <h3>Origin: ${origin.name}</h3>
+          <h3>Last Location: ${location.name}</h3>
+        </article>
+      </div>
+    `;
   return view;
-}
+};
 
 export default Character;
