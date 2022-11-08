0cd477e7ae5ee2736d56f29f8ad43e312e4d16e7
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2022-11-08 08:41:10 -0500

    config webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugins = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugins([
+      {
+        inject: true,
+        template: "./public/index.html",
+        filename: "./index.html",
+      },
+    ]),
+  ],
+};
