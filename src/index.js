68b206f3d696f099a77801369ecad5dd117e7211
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2022-11-08 08:42:22 -0500

    add entry point index.js and create tmplates

---
+++ src/index.js
@@ -0,0 +1,5 @@
+function saluda(h) {
+  console.log("hola", h);
+}
+
+saluda("hola nanas");
---
+++ src/pages/Character.js
@@ -0,0 +1,26 @@
+const Character = () => {
+  const view = `
+  
+  <div class="Character-inner">
+      <article class="Characters-card">
+          <img src="image" alt="name">
+      </article>
+      <h2>Name</h2>
+
+      <article class="Character-card">
+          <h3>Episode:</h3>
+          <h3>Status:</h3>
+          <h3>Species:</h3>
+          <h3>Gender:</h3>
+          <h3>Origin:</h3>
+          <h3>Last Location:</h3>
+
+      </article>
+
+  </div>
+  
+  `
+  return view;
+}
+
+export default Character;
