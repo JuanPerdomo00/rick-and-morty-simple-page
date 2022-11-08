0982b11f6945696c2f1b01e0bad63989b2f9f408
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2022-11-08 14:54:22 -0500

    se mejoror el Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,25 @@
-const Home = () => {
-  const view = `
-  <div class="Characters">
-  <article class="Character-item">
-      <a href="#/1/">
-        <img src="image" alt="name">
-        <h2>Name</h2>
-      </a>
-  </article>
-<div>
-`;
+import { getData } from "../utils/getData";
 
+const Home = async () => {
+  const characters = await getData();
+  const view = `
+    <div class="Characters">
+
+    ${characters.results
+      .map(
+        (character) => ` 
+      <article class="Character-item">
+          <a href="#/${character.id}/">
+          <img src="${character.image}" alt="${character.name}">
+          <h2>${character.name}</h2>
+        </a>
+    </article>
+    
+    `
+      )
+      .join("")}
+    </div>
+  `;
   return view;
 };
 
